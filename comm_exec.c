4c7b0caea9accec7a388a4d19d56ee56d72c4b4a
root <root@DESKTOP-7CAO994.>
2025-03-29 11:29:06 +0100

    2025-03-29 11:29:05

--- comm_exec.c
+++ comm_exec.c
@@ -3,11 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   comm_exec.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aingunza <aingunza@student.42.fr>          +#+  +:+       +#+        */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 18:24:35 by root              #+#    #+#             */
-/*   Updated: 2025/03/26 16:23:05 by aingunza         ###   ########.fr       */
+/*   Updated: 2025/03/27 21:11:45 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+void exec(char *cmd, char **env)
+{
+	char **s_cmd;
+}
+
+int pipe(int fd[2])
+{
+	pid_t pid = fork();
+
+	if (pid < 0)
+	{
+		ft_printf("fork failed");
+		return 1;		
+	}
+	else if(pid == 0)
+		ft_printf("hijo")
+	
+	read(fd[0])
+	write(fd[1])
+}
